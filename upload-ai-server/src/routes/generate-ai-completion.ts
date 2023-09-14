871655ebe5d8e94bc97da2b5458e85895f83dbc7
brunoadller <adller676@gmail.com>
2023-09-14 19:47:41 -0300

    finalizacao do nlw-ia

--- upload-ai-server/src/routes/generate-ai-completion.ts
+++ upload-ai-server/src/routes/generate-ai-completion.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance } from "fastify";
 import { createReadStream } from 'fs';
 import {z} from 'zod'
+import { streamToResponse, OpenAIStream } from 'ai';
 import {prisma} from '../lib/prisma';
 import { openai } from '../lib/openai';
 
@@ -10,12 +11,12 @@ export async function generateAICompletionRoute(app: FastifyInstance){
  app.post('/ai/complete', async (req, reply) => {
   const bodySchema = z.object({
     videoId: z.string().uuid(),
-    template: z.string(),
+    prompt: z.string(),
     temperature: z.number().min(0).max(1).default(0.5)
   })
 
   
-  const { videoId, template, temperature } = bodySchema.parse(req.body)
+  const { videoId, prompt, temperature } = bodySchema.parse(req.body)
 
   //buscando os dados do video para enviar a transcrição para openai resumir
   const video = await prisma.video.findUniqueOrThrow({
@@ -27,7 +28,7 @@ export async function generateAICompletionRoute(app: FastifyInstance){
     return reply.status(400).send({server: 'Video transcription was not generated yet.'})
   }
   //enviando a transcrição, ou seja, trocando a transcription do template pela video transcription
-  const promptMessage = template.replace('{transcription}', video.transcription)
+  const promptMessage = prompt.replace('{transcription}', video.transcription)
   
   //define qual gpt sera usado para criar o resumo a partir da trascrição
   const response = await openai.chat.completions.create({
@@ -35,9 +36,18 @@ export async function generateAICompletionRoute(app: FastifyInstance){
     temperature,
     messages: [
       {role: 'user', content: promptMessage} //passa o texto junto como oque quer que seja o resumo
-    ]
+    ],
+    stream: true,
+
   })
-  
-  return {response }
+    //procedimento para fazer o retorno ser igual ah do chat gpt
+   const stream = OpenAIStream(response)
+
+   streamToResponse(stream, reply.raw, {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS'
+    },
+   } )
  })
 }
